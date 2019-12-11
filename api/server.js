577fb189395368cb60424b32928424f501002876
spenclark <spenclark@hotmail.com>
2019-12-10 22:27:10 -0800

    router /api/cars established

--- api/server.js
+++ api/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
 const helmet = require('helmet')
-// const carRouter = require('./router.js')
+const router = require('./router.js')
 
 const server = express();
 
@@ -10,7 +9,7 @@ server.use(helmet())
 server.use(express.json())
 server.use(morgan())
 
-// server.use('/api/cars', router)
+server.use('/api/cars', router)
 
 server.get('/', (req,res) => {
     res.status(200).json(`Server is sane`)
